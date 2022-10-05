deddd21e52da2534e9da20ecbc9c93e394b25d5a
calvinbeschfromclarksonuniversity <beschcr@clarkson.edu>
2022-10-05 01:59:34 +0000

    java docc

--- src/main/ClackClient.java
+++ src/main/ClackClient.java
@@ -19,7 +19,7 @@ public class ClackClient {
     * Full constructor for clack client
     * @param userName, the name of the client using clack
     * @param hostName, the name of the device being used by the client
-    * @param port the netwoeking poet used to connect to the server
+    * @param port the networking poet used to connect to the server
     */
     
    public ClackClient(String userName, String hostName, int port){
@@ -87,16 +87,25 @@ public class ClackClient {
     }
     
       /**
-    * @param port the netwoeking poet used to connect to the server
+    * @return port the networking port used to connect to the server
     */
     public int getPort(){
         return this.port;
     }
+
+    /**
+     * @param other ClackClient to be compared
+     * @return true if the passed ClackClient is considered equal to this one (I.E. has the same values for all of the
+     * instance variables.)
+     */
     public boolean equals(Object other){
       ClackClient client = (ClackClient)other;
       return ((this.userName == client.userName) && (this.hostName == client.hostName) && (this.port == client.port) && (this.dataToRecieveFromServer == client.dataToRecieveFromServer) && (this.dataToSendToServer == client.dataToSendToServer));
-
     }
+
+    /**
+     * @return a string displaying the values all of the instance variables of this ClackClient
+     */
     public String toString() {
         return "The username of the client is " + this.userName + "\n" +
                 "The host name of the client is " + this.hostName + "\n" +
@@ -105,8 +114,16 @@ public class ClackClient {
                 "The data to send to the server is : " + this.dataToSendToServer + "\n\n";
     }
 
+    /**
+     * Returns an integer that will be the same as that of another ClackClient considered equal (see
+     * ClackClient#equals), but different from that of another ClackClient considered not equal. Implemented by
+     * constructing a string that has these properties itself, using information from each instance variable and a
+     * separator that cannot be sent in messages, and then taking the hash code of that string.
+     * NOTE: If ` must be able to be sent in messages, a different separator may be chosen.
+     * @return the hash code of this instance of ClackClient
+     */
     public int hashCode(){
       String uniqueString = userName + "`" + hostName + "`" + port + "`" + closeConnection + "`" + dataToSendToServer.hashCode() + "`" + dataToRecieveFromServer.hashCode();
-        return uniquestring.hashCode();
+        return uniqueString.hashCode();
     }
 }
