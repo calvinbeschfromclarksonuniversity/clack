4c68cb653ccf5f4deed1b4427ad9ef057e70f020
calvinbeschfromclarksonuniversity <beschcr@clarkson.edu>
2022-10-30 22:37:41 +0000

    added encryption and decryption capabilities

--- src/data/ClackData.java
+++ src/data/ClackData.java
@@ -64,6 +64,52 @@ public abstract class ClackData {
 	}
 
 	/**
+	 * Encrypts the given message using the Vignere cipher with the given key. Returns the result.
+	 * @param inputStringToEncrypt message to encrypt
+	 * @param key key to encrypt with
+	 * @return encrypted message
+	 */
+	protected String encrypt(String inputStringToEncrypt, String key) {
+		String encrypted = "";
+		int keyIndex = 0;
+
+		for (int i = 0; i < inputStringToEncrypt.length(); i++) {
+			if(65 <= inputStringToEncrypt.charAt(i) && inputStringToEncrypt.charAt(i) <= 90) {
+				encrypted += (char) (((inputStringToEncrypt.charAt(i) - 65 + Character.toUpperCase(key.charAt(keyIndex)) - 65) % 26) + 65);
+				keyIndex = (keyIndex + 1) % key.length();
+			}
+			else if (97 <= inputStringToEncrypt.charAt(i) && inputStringToEncrypt.charAt(i) <= 122) {
+				encrypted += (char) (((inputStringToEncrypt.charAt(i) - 97 + Character.toUpperCase(key.charAt(keyIndex)) - 65) % 26) + 97);
+				keyIndex = (keyIndex + 1) % key.length();
+			}
+			else encrypted += inputStringToEncrypt.charAt(i);
+		}
+		return encrypted;
+	}
+
+	/**
+	 * Decrypts the given (encrypted) message using the given key. Returns the result.
+	 * @param inputStringToDecrypt encrypted message to decrypt
+	 * @param key key to decrypt with
+	 * @return decrypted message
+	 */
+	protected String decrypt(String inputStringToDecrypt, String key) {
+		String decrypted = "";
+		int keyIndex = 0;
+
+		for(int i = 0; i < inputStringToDecrypt.length(); i++) {
+			if (65 <= inputStringToDecrypt.charAt(i) && inputStringToDecrypt.charAt(i) <= 90) {
+				decrypted += (char) (((inputStringToDecrypt.charAt(i) - 65 - (Character.toUpperCase(key.charAt(keyIndex)) - 65) + 26) % 26) + 65);
+				keyIndex = (keyIndex + 1) % key.length();
+			} else if (97 <= inputStringToDecrypt.charAt(i) && inputStringToDecrypt.charAt(i) <= 122) {
+				decrypted += (char) (((inputStringToDecrypt.charAt(i) - 97 - (Character.toUpperCase(key.charAt(keyIndex)) - 65) + 26) % 26) + 97);
+				keyIndex = (keyIndex + 1) % key.length();
+			} else decrypted += inputStringToDecrypt.charAt(i);
+		}
+		return decrypted;
+	}
+
+	/**
 	 * @return the type of packet
 	 */
 	public int getType() {
